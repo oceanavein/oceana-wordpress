31007b0a4468b44e22b99560033eebbd931657e0
Brad Sawicki <git@bradsawicki.com>
2017-08-01 21:44:24 +0200

    Office/Symptoms/Treatments ACF/tpl

---
+++ wp-content/themes/oceana/page-office.php
@@ -0,0 +1,7 @@
+<?php /* Template Name: Our Office */ ?>
+
+<?php get_header(); ?>
+
+	<h1><?php the_title(); ?></h1>
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/oceana/page-treatments.php
@@ -0,0 +1,7 @@
+<?php /* Template Name: Treatments */ ?>
+
+<?php get_header(); ?>
+
+	<h1><?php the_title(); ?></h1>
+
+<?php get_footer(); ?>
