0e269c8eb268c1b005f24073905e4259e0e97d81
Brad Sawicki <git@bradsawicki.com>
2017-08-03 19:56:52 +0200

    Editor user edit theme

--- wp-content/themes/oceana/functions.php
+++ wp-content/themes/oceana/functions.php
@@ -101,3 +101,11 @@ function get_default_blog_image($size=null){
 if( function_exists('acf_add_options_page') ) {
 	acf_add_options_page();
 }
+
+
+// add editor the privilege to edit theme
+
+// get the the role object
+$role_object = get_role( 'editor' );
+// add $cap capability to this role object
+$role_object->add_cap( 'edit_theme_options' );
