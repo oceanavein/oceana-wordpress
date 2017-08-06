f5dba10627818da2979d91c5ef0785a5dd719e01
Brad Sawicki <git@bradsawicki.com>
2017-08-06 16:42:17 +0200

    Treatments Hero Image ACF markup

--- wp-content/themes/oceana/page-treatments.php
+++ wp-content/themes/oceana/page-treatments.php
@@ -31,7 +31,8 @@
 </section>
 
 <section class="treatments-image">
-  <img src="" alt="Oceana Treatments" />
+	<?php $image = get_field('hero_image'); ?>
+	<img src="<?php echo $image["sizes"]['full-width-image']; ?>" srcset="<?php echo $image["sizes"]['full-width-image']; ?> 1000w, <?php echo $image["sizes"]['full-width-image-2x']; ?> 2000w" alt="Oceana Vein Clinic Office" />
 </section>
 
 <section class="treatments-list">
