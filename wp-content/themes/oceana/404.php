9a671035ff8c8b4232279bc55c9e2bf3c04c9be4
Brad Sawicki <git@bradsawicki.com>
2017-09-12 11:28:53 +0200

    404 page markup/styles

--- wp-content/themes/oceana/404.php
+++ wp-content/themes/oceana/404.php
@@ -1,5 +1,17 @@
 <?php get_header(); ?>
 
-	<h1>Whoops!</h1>
+<div class="page-wrapper">
+	<div class="container">
+
+		<h1 class="page-title title__h1">Page Not Found</h1>
+
+		<div class="body-content">
+			<p>Return Home and try again.</p>
+		</div>
+
+		<p><a href="/" class="btn btn--outline">Home</a></p>
+
+	</div>
+</div>
 
 <?php get_footer(); ?>
