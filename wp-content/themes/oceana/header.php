464405b502a86978192c834daf7baa41057e1c5e
Brad Sawicki <git@bradsawicki.com>
2018-01-16 15:40:11 +0100

    Package lock json / Hide Lang switch / Increase width home hero list

--- wp-content/themes/oceana/header.php
+++ wp-content/themes/oceana/header.php
@@ -27,11 +27,11 @@
 			<div class="logo">
 				<a href="<?php echo pll_home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/logo.svg" alt="Oceana Vein Clinic" /></a>
 			</div>
-			<nav class="language-nav">
+			<!-- <nav class="language-nav">
 				<ul>
 					<?php pll_the_languages();?>
 				</ul>
-			</nav>
+			</nav> -->
 			<nav class="main-nav">
 				<button class="mobile-btn btn btn--outline">
 					<span class="mobile-btn__label">Menu</span>
