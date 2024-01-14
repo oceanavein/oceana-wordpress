b00d63065f26a4a0fb8ebec623c04697ede431bf
Brad Sawicki <git@bradsawicki.com>
2024-01-14 12:57:52 +0100

    Update npm modules, add FB pixel snippet

--- wp-content/themes/oceana/header.php
+++ wp-content/themes/oceana/header.php
@@ -5,6 +5,25 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta content="width=device-width, initial-scale=1.0, user-scalable=1, minimum-scale=1.0" name="viewport">
 	<?php wp_head(); ?>
+	<!-- Meta Pixel Code -->
+	<script>
+	!function(f,b,e,v,n,t,s)
+	{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+	if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+	n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];
+	s.parentNode.insertBefore(t,s)}(window,document,'script',
+	'https://connect.facebook.net/en_US/fbevents.js');
+	fbq('init', '896419075303815');
+	fbq('track', 'PageView');
+	</script>
+	<noscript>
+	<img height="1" width="1"
+	src="https://www.facebook.com/tr?id=896419075303815&ev=PageView
+	&noscript=1"/>
+	</noscript>
+	<!-- End Meta Pixel Code -->
 	<script>
 	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
