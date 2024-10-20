1b3c6e16eccc686df3f2c7d0ab58e408fe137f92
Brad Sawicki <git@bradsawicki.com>
2024-10-20 14:39:25 +0200

    Single post hero, appt form sidebar

--- wp-content/themes/oceana/single.php
+++ wp-content/themes/oceana/single.php
@@ -3,25 +3,41 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 	<article class="post">
-		<div class="container">
+		<div class="hero relative overflow-hidden bg-blue-pale">
+			<div class="container relative z-10">
 
-			<p class="mb-24">
-				<a href="<?php echo get_bloginfo( 'url' ); ?>/blog/" class="flex items-center">
-					<svg class="w-6 h-6 mr-8">
-						<use xlink:href="#icon-arrow-left" />
-					</svg>
-					Blog
-				</a>
-			</p>
+				<p class="mb-24">
+					<a href="<?php echo get_bloginfo( 'url' ); ?>/blog/" class="flex items-center">
+						<svg class="w-6 h-6 mr-8">
+							<use xlink:href="#icon-arrow-left" />
+						</svg>
+						Blog
+					</a>
+				</p>
 
-			<h1 class="text-h1 mb-6">
-				<?php the_title(); ?>
-			</h1>
-
-			<div class="body-content">
-				<?php the_content(); ?>
+				<h1 class="text-h1 mb-6">
+					<?php the_title(); ?>
+				</h1>
 			</div>
 
+			<div class="hero-waves">
+				<?php get_template_part( 'partials/svg/bg-waves' ); ?>
+			</div>
+		</div>
+
+
+		<div class="container">
+			<div class="grid grid-cols-3 gap-10">
+
+				<div class="col-span-2 xl:pr-64 body-content">
+					<?php the_content(); ?>
+				</div>
+
+				<div>
+					<?php get_template_part('partials/appointment-form'); ?>
+				</div>
+
+			</div>
 		</div>
 	</article>
 
