c770f8b46b90ee82f10123cebba611e1b72d4aa6
Brad Sawicki <git@bradsawicki.com>
2024-10-21 08:53:28 +0200

    Main nav z index 20

--- wp-content/themes/oceana/page-treatments.php
+++ wp-content/themes/oceana/page-treatments.php
@@ -7,7 +7,9 @@
 
 		<div class="md:flex gap-5">
 			<div class="col-span-8">
-				<h1 class="page-title text-h1"><?php the_title(); ?></h1>
+				<h1 class="page-title text-h1">
+					<?php the_title(); ?>
+				</h1>
 				<div class="section-content">
 					<?php the_field('hero_content'); ?>
 				</div>
@@ -22,7 +24,7 @@
 			<a href="#treatments-list" id="treatments-scroll">
 				<span class="anchor-link__label">View Treatments</span>
 				<svg class="icon-arrow-down">
-					<use xlink:href="#icon-arrow-down"></use>
+					<use xlink:href="#icon-arrow-down" />
 				</svg>
 			</a>
 		</div>
@@ -32,10 +34,12 @@
 
 <section class="treatments-image">
 	<?php $image = get_field('hero_image'); ?>
-	<img src="<?php echo $image["sizes"]['full-width-image']; ?>"
-				srcset="<?php echo $image["sizes"]['full-width-image']; ?> 1000w,
-				<?php echo $image["sizes"]['full-width-image-2x']; ?> 2000w"
-				alt="Oceana Vein Clinic Office" />
+	<img
+		src="<?php echo $image["sizes"]['full-width-image']; ?>"
+		srcset="<?php echo $image["sizes"]['full-width-image']; ?> 1000w,
+						<?php echo $image["sizes"]['full-width-image-2x']; ?> 2000w"
+		alt="Oceana Vein Clinic Office"
+	/>
 </section>
 
 <section id="treatments-list" class="treatments-list">
@@ -45,10 +49,16 @@
 			<div class="grid md:grid-cols-2 gap-10 treatments-grid">
 		    <?php while ( have_rows('treatments') ) : the_row(); ?>
 					<div class="mb-24">
-		        <h2 class="text-h3"><?php the_sub_field('title'); ?></h2>
+		        <h2 class="text-h3">
+							<?php the_sub_field('title'); ?>
+						</h2>
 		        <div class="body-content">
 		          <?php the_sub_field('description'); ?>
-		          <p><a href="<?php the_sub_field('page_link'); ?>" class="cta-link">Learn More</a></p>
+		          <p>
+								<a href="<?php the_sub_field('page_link'); ?>" class="cta-link">
+									Learn More
+								</a>
+							</p>
 		        </div>
 		      </div>
 		    <?php endwhile; ?>
@@ -63,9 +73,13 @@
 
 		<div class="md:flex gap-10">
 			<div class="col-span-6">
-				<h3 class="section-title text-h2"><?php the_field('payment_section_title'); ?></h3>
+				<h3 class="section-title text-h2">
+					<?php the_field('payment_section_title'); ?>
+				</h3>
 				<div class="body-content">
-		      <p><?php the_field('payment_section_content'); ?></p>
+		      <p>
+						<?php the_field('payment_section_content'); ?>
+					</p>
 				</div>
 			</div>
 
